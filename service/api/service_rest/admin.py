ea8c82fc9ae35004b79a0d66a4f4198dcb586d1e
Lesley <tomosadalesley@gmail.com>
2022-10-24 17:47:26 -0700

    models created and registered in admin

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import AutomobileVO, ServiceAppointment, Technician
+
 # Register your models here.
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Technician)
+class TechnicianAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ServiceAppointment)
+class ServiceAppointmentAdmin(admin.ModelAdmin):
+    pass
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    import_href = models.CharField(max_length=200, unique=True)
+
+class Technician(models.Model):
+    name=models.CharField(max_length=100)
+    employee_number = models.PositiveSmallIntegerField()
+
+class ServiceAppointment(models.Model):
+    vin=models.CharField(max_length=17)
+    owner_name = models.CharField(max_length = 100)
+    date_and_time=models.DateTimeField()
+    technician = models.ForeignKey(Technician, related_name="technician",on_delete=models.PROTECT,)
+    service_reason=models.CharField(max_length=200)
+    is_vip=models.BooleanField(default=False)
+    is_completed=models.BooleanField(default=False)
